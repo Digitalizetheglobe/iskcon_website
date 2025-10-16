24612db36bfdeac99070f50895244c4df270073e
sangmesh-18 <garibesangmesh@gmail.com>
2025-10-16 12:41:08 +0530

    made changes for navigation

--- app/[campaign]/page.tsx
+++ app/[campaign]/page.tsx
@@ -8,6 +8,31 @@ export default async function CampaignRedirectPage({
 }) {
   const { campaign } = await params;
 
+  // Exclude specific routes that should not be treated as campaigns
+  const excludedCampaigns = [
+    'gallery',
+    'about-us',
+    'contact',
+    
+    'blog',
+    'events',
+    'volunteer',
+    'governance',
+    'policies',
+    'terms-conditions',
+    'trust',
+    'refund',
+    'certificates',
+    'our-initiative',
+    'home',
+    
+  ];
+
+  // If this is an excluded route, don't redirect
+  if (excludedCampaigns.includes(campaign)) {
+    return null;
+  }
+
   redirect(`/?utm_source=AIKYA&utm_medium=SMS&utm_campaign=${campaign}`);
 
   // Returning null satisfies React.FC typing
